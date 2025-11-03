27b5970c9e5497c6e442dae17108055be96552a9
AttachShell <tascheel.c@proton.me>
2025-11-03 15:51:13 -0700

    more test

--- button.py
+++ button.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 
 def init(pin):
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(pin, GPIO.BOTH, callback=detect)
+    GPIO.add_event_detect(pin, GPIO.BOTH, callback=detect, bouncetime=200)
 
 def detect(change):
-    print("button: " + change)
+    print("button: " + change + ", " + GPIO.input(12))
 
