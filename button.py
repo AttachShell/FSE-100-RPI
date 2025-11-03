673e9dfe1237e5db91f0c5bc82c5c2c234127d01
AttachShell <tascheel.c@proton.me>
2025-11-03 16:18:34 -0700

    Wee?

--- button.py
+++ button.py
@@ -5,5 +5,5 @@ def init(pin):
     GPIO.add_event_detect(pin, GPIO.BOTH, callback=detect, bouncetime=200)
 
 def detect(change):
-    print("button: " + change + ", " + GPIO.input(15))
+    print(change)
 
